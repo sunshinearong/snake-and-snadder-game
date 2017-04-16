2336586da0b20a9a142b70b3992763360766fdc2
sunshinearong <1241372826@qq.com>
2017-04-16 17:06:57 +0800

    Update salbnode.py

--- salbnode.py
+++ salbnode.py
@@ -1,24 +1,4 @@
-"""
-# Copyright Nick Cheng, 2016
-# Distributed under the terms of the GNU General Public License.
-#
-# This file is part of Assignment 1, CSCA48, Winter 2017
-#
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this file.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
-# Do not change the declaration of SALBnode class.
 
 class SALBnode:
     """Node for linked list representation of snakes and ladders board."""
