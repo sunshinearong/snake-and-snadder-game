ce640d4465fed639673aa51151f2feaf0a7acdef
sunshinearong <1241372826@qq.com>
2017-04-16 17:05:20 +0800

    Update salb2salbLL.py

--- salb2salbLL.py
+++ salb2salbLL.py
@@ -1,27 +1,7 @@
-"""
-# Copyright Nick Cheng, 2016, Greg (Hidayat) Mehdiyev
-# Distributed under the terms of the GNU General Public License.
-#
-# This file is part of Assignment 1, CSCA48, Winter 2017
-#
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this file.  If not, see <http://www.gnu.org/licenses/>.
-"""
 
 from salboard import SALboard
 from salbnode import SALBnode
 
-# Add your functions here.
 
 def salb2salbLL(salb):
         
@@ -88,9 +68,7 @@ def salb2salbLL(salb):
 
     
     
-    # this code tests your salb2salbLL function by printing a visual representation
-    # of your game board! Look at the example in the piazza post to see what it
-    # should look like
+    
 if __name__ == "__main__":
         import math
         import doctest
